ae666cbea50bbff73c07a466ed63de5c3c46af2d
Cyrsztof <cyrsztof@gmail.com>
2017-05-13 20:29:56 +0200

    Testowanie ncurses, Makefile

---
+++ cmake-build-debug/CMakeFiles/SO_projekt.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SO_projekt.dir/main.cpp.o"
+  "SO_projekt.pdb"
+  "SO_projekt"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SO_projekt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
